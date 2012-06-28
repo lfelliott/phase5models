ab3b5d26d8c362d924823b060ec40638804c913e
lfelliott <lfelliott@hotmail.com>
2012-06-28 13:13:03 -0500

    put work in function to pass subsets

--- correct_cities.py
+++ correct_cities.py
@@ -5,19 +5,22 @@ citylayer = "l_cities"
 DM.MakeFeatureLayer(cities, citylayer)
 print "Making city layer."
 
-p5_working = "C:\\WorkSpace\\Phase5\\Objects\\p5_working_test.gdb\\north_test6"
-layername = "l_p5_north_test6"
-DM.MakeFeatureLayer(p5_working, layername)
-print "Making working layer"
+def applycities(fc):
+	p5_working = "C:\\WorkSpace\\Phase5\\Objects\\p5_working_test.gdb\\%s" % fc
+	layername = "l_p5_%s" % fc
+	print "Making working layer"
+	DM.MakeFeatureLayer(p5_working, layername)
+	print "selecting by location"
+	DM.SelectLayerByLocation(layername, "COMPLETELY_WITHIN", citylayer, "", "NEW_SELECTION")
+	print "selecting by attribute"
+	DM.SelectLayerByAttribute(layername, "SUBSET_SELECTION", selectstr)
+	if (int(str(DM.GetCount(layername))) > 0):
+		print "calculating on " + str(DM.GetCount(layername)) + " objects."
+#		DM.CalculateField(layername, "lulc", 25)
+		DM.CalculateField(layername, "VegNum", 9411)
+	else:
+		print "nothing to calculate"
 
-selectstr = "\"lulc\" = 1 or \"lulc\" = 3 OR \"lulc\" = 5 or \"lulc\" = 19 or \"lulc\" = 31 or \"lulc\" =27"
-print selectstr
-print "selecting by location"
-DM.SelectLayerByLocation(layername, "COMPLETELY_WITHIN", citylayer, "", "NEW_SELECTION")
-print "selecting by attribute"
-DM.SelectLayerByAttribute(layername, "SUBSET_SELECTION", selectstr)
-if (int(str(DM.GetCount(layername))) > 0):
-	print "calculating"
-	DM.CalculateField(layername, "lulc", 25)
-else:
-	print "nothing to calculate"
+fcs = ["west_test5", "south_test5", "north_test5"]
+for item in fcs:
+	applycities(item)
