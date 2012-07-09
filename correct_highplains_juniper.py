d01987c0ccd6968bf01f843fb7fb64c31619c58e
lfelliott <lfelliott@hotmail.com>
2012-07-09 16:00:40 -0500

    Added fixing saltcedar on EP and Caprock to invasive juniper.

--- correct_highplains_juniper.py
+++ correct_highplains_juniper.py
@@ -8,7 +8,7 @@ print "Making juniper layer."
 selectstr = "(\"VegNum\" = 9101 or \"VegNum\" = 9105) and  \"epa_ecoreg\" like '25%'"
 print selectstr
 
-def applycities(fc):
+def applyjuniper(fc):
 	p5_working = "C:\\WorkSpace\\Phase5\\Objects\\p5_working_test.gdb\\%s" % fc
 	layername = "l_p5_%s" % fc
 	print "Making working layer for " + fc
@@ -23,6 +23,24 @@ def applycities(fc):
 		print "nothing to calculate"
 	print "selecting by location"
 
-fcs = ["north_test5"]
+def fixsaltcedar(fc):
+	# this is mostly on riparian or loamy, clay loam etc.
+	selectstr2 = "(\"lulc\" = 11 or \"lulc\" = 19) and \"EcoGroup\" not like '%Bottomland%' and \"VegNum\" = 9204 and (\"epa_ecoreg\" like '30%' or \"epa_ecoreg\" like '26%' or \"epa_ecoreg\" = '24e')"
+	p5_working = "C:\\WorkSpace\\Phase5\\Objects\\p5_working_test.gdb\\%s" % fc
+	layername = "l_p5_%s" % fc
+	print "Making working layer for " + fc
+	DM.MakeFeatureLayer(p5_working, layername)
+	print "selecting by attribute"
+	DM.SelectLayerByAttribute(layername, "NEW_SELECTION", selectstr2)
+	if (int(str(DM.GetCount(layername))) > 0):
+		print "calculating on " + str(DM.GetCount(layername)) + " objects."
+		DM.CalculateField(layername, "VegNum", 9105)
+	else:
+		print "nothing to calculate"	
+	
+applyjuniper("north_test5")
+
+
+fcs = ["north_test5", "south_test5"]
 for item in fcs:
-	applycities(item)
+	fixsaltcedar(item)
